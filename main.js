b07078946ca52f3f91b43f9f10752355857287a4
Chris Woodard <c.woodarda@Chriss-MBP.MG8702>
2021-07-06 15:03:21 -0500

    hero-rebuild-mobile-vh-solution

---
+++ main.js
@@ -0,0 +1,14 @@
+// $("main").css("background", "red");
+// document.querySelector("main").style.background = "red";
+
+// let viewPortSize = window.innerHeight;
+// window.addEventListener("touchmove", () => {
+//   if (viewPortSize > 667) {
+//     console.log("it changed");
+//   }
+// });
+
+// $(window).
+// if (viewPortSize > 667) {
+//   document.querySelector("main").style.background = "red";
+// }
