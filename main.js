01bc0b545f109c452f20e51021e998b02b501ad7
Chris Woodard <c.woodarda@Chriss-MacBook-Pro.local>
2021-07-15 00:12:09 -0400

    mobile-media-query-dev-ongoing

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// $("main").css("background", "red");
-// document.querySelector("main").style.background = "red";
-
-// let viewPortSize = window.innerHeight;
-// window.addEventListener("touchmove", () => {
-//   if (viewPortSize > 667) {
-//     console.log("it changed");
-//   }
-// });
-
-// $(window).
-// if (viewPortSize > 667) {
-//   document.querySelector("main").style.background = "red";
-// }
-
 window.addEventListener("resize", () => {
   document
     .querySelector(":root")
