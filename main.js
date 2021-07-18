55e356cbb5e51c3a2faa6466aa4186552523e17f
Chris Woodard <c.woodarda@Chriss-MBP.MG8702>
2021-07-18 16:46:45 -0500

    mobile-media-query-hero-rebuild

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-window.addEventListener("resize", () => {
-  document
-    .querySelector(":root")
-    .style.setProperty("--vh", window.innerHeight / 100 + "px");
-});
+// window.addEventListener("resize", () => {
+//   document
+//     .querySelector(":root")
+//     .style.setProperty("--vh", window.innerHeight / 100 + "px");
+// });
